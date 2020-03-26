76e06e786fd0bf9b50018a75793ae1584e430f7b
Haikel Fazzani <haikel.fazzani@hotmail.com>
2020-03-26 20:54:19 +0100

    update app

--- src/containers/Transpiler.js
+++ src/containers/Transpiler.js
@@ -6,11 +6,16 @@ export default function Transpiler ({ input, codeType }) {
   const [transpiledCode, setTranspiledCode] = useState();
 
   useEffect(() => {
-    let output = window.Babel.transform(input, {
-      envName:'production',presets: codeType === 'react' ? ['react', 'es2015'] : ['es2015']
-    }).code;
-    
-    setTranspiledCode(output);
+    let output = '';
+    try {
+      output = window.Babel.transform(input, {
+        envName:'production',presets: codeType === 'react' ? ['react', 'es2015'] : ['es2015']
+      }).code;
+      
+      setTranspiledCode(output);      
+    } catch (error) {
+      setTranspiledCode(error.message);
+    }
   }, [input]);
 
   return <Editor value={transpiledCode} />;
